5d6a6902e2ea6979dce359b3ff4aefaec8b3fdc5
PavelSbvt <sablinpavel33@gmail.com>
2026-08-02 01:23:11 +0300

    + [py] create file with functions on Rich

---
+++ src/utils/output_rich.py
@@ -0,0 +1,35 @@
+from rich.console import Console
+from rich.panel import Panel
+import shutil
+
+
+console = Console(width=200)
+
+print(f"Ширина консоли: {shutil.get_terminal_size().columns}")
+
+def log_error(message: str, details: str = "") -> None:
+    """
+    Вывод ошибки с панелью
+    """
+
+    content = f"[bold white on red] ⚠️ ОШИБКА ⚠️ [/bold white on red]\n\n{message}"
+    if details:
+        content += f"\n\n[dim]{details}[/dim]"
+
+    console.print(
+        Panel(
+            content,
+            title="[red]Ошибка[/red]",
+            border_style="red",
+            padding=(1, 2)
+        )
+    )
+
+def simple_log(message: str) -> None:
+    """
+    Логгирование с rich
+    :param message:
+    :return: None
+    """
+
+    console.log(f"[green]{message}[/green]")
