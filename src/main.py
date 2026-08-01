c86cd128f6c649ee1d940c9f13bd6dca3837dee8
PavelSbvt <sablinpavel33@gmail.com>
2026-08-02 01:10:21 +0300

    + [py] create main.py

---
+++ src/main.py
@@ -0,0 +1,36 @@
+import sys
+from pathlib import Path
+
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
+
+from gui.loading_start_window import MyLoadingWindow
+
+
+def main() -> None:
+    """
+    Функция, запускающая загрузочное окно, которое по прошествии 3 секунд закроется,
+     после чего откроется основное окно программы. Функция-связка.
+    """
+
+    app = QApplication(sys.argv)
+    icon_path = Path('resources/images/icons/icon_pdf_maker_255_size.ico')
+    app.setWindowIcon(QIcon(f"{icon_path.as_posix()}"))
+    loading_win = MyLoadingWindow()
+    loading_win.show()
+
+    def on_loading_closed() -> None:
+        """
+        Функция, которая показывает основное окно,
+         а загрузочное окно скрывает при показе основного окна программы.
+        """
+
+        main_win = MyWindow()
+        main_win.show()
+        loading_win.hide()
+
+    loading_win.closed.connect(on_loading_closed)
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
