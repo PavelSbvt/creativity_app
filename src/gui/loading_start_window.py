2bcec53db36ba031a1e5a4fa597aa983c00150d3
PavelSbvt <sablinpavel33@gmail.com>
2026-08-19 01:37:05 +0300

    > [py] change function for animation gradient (X2)

--- src/gui/loading_start_window.py
+++ src/gui/loading_start_window.py
@@ -241,8 +241,8 @@ class MyLoadingWindow(QWidget):
 
         self.background_window_by_time_of_day()
 
-        self.gradient_1_pos = 0.15
-        self.gradient_2_pos = 0.3
+        self.gradient_1_pos = 0.4
+        self.gradient_2_pos = 0.55
 
         self.opacity_animation = 0.05
 
@@ -271,8 +271,8 @@ class MyLoadingWindow(QWidget):
 
         self.change_color += 0.5 * self.change_color_direction
 
-        if self.change_color >= 80:
-            self.change_color = 80
+        if self.change_color >= 40:
+            self.change_color = 40
             self.change_color_direction = -1
         elif self.change_color <= 1:
             self.change_color = 1
@@ -287,22 +287,24 @@ class MyLoadingWindow(QWidget):
         if not self.flag_finish_animation:
             self.widget_gradient.setStyleSheet(f"""
                     background: qlineargradient(
-                        x1: 0, y1: 0.75, x2: 1, y2: 0.45,
+                        x1: 0, y1: 0.5, x2: 1, y2: 0,
                         stop: 0 rgba({r}, {g}, {b}, 0.0),
                         stop: {self.gradient_1_pos + pos * 0.3} rgba({r}, {g}, {b}, {self.opacity_animation:.2f}),
-                        stop: {self.gradient_2_pos + pos * 0.3} rgba({change_red * 0.5:.0f}, {change_green * 0.5:.0f}, {change_blue * 0.5:.0f}, {self.opacity_animation * 4:.2f}),
-                        stop: 1 rgba({change_red}, {change_green}, {change_blue}, {self.opacity_animation * 8:.2f})
+                        stop: {self.gradient_2_pos + pos * 0.3} rgba({change_red * 0.5:.0f}, {change_green * 0.5:.0f}, {change_blue * 0.5:.0f}, {self.opacity_animation * 3:.2f}),
+                        stop: 1 rgba({change_red}, {change_green}, {change_blue}, {self.opacity_animation * 5:.2f})
                     );
                     border-radius: 10px;
                 """)
         else:
             self.widget_gradient.setStyleSheet(f"""
                     background: qlineargradient(
-                        x1: 0, y1: 0.75, x2: 1, y2: 0.45,
+
+                        x1: 0, y1: 0.5, x2: 1, y2: 0,
+                        
                         stop: 0 rgba({r}, {g}, {b}, 0.0),
                         stop: {self.gradient_1_pos + pos * 0.3} rgba({r}, {g}, {b}, {self.opacity_animation * 1:.2f}),
-                        stop: {self.gradient_2_pos + pos * 0.3} rgba({change_red * 0.5:.0f}, {change_green * 0.5:.0f}, {change_blue * 0.5:.0f}, {self.opacity_animation * 4:.2f}),
-                        stop: 1 rgba({change_red}, {change_green}, {change_blue}, {self.opacity_animation * 8:.2f})
+                        stop: {self.gradient_2_pos + pos * 0.3} rgba({change_red * 0.5:.0f}, {change_green * 0.5:.0f}, {change_blue * 0.5:.0f}, {self.opacity_animation * 3:.2f}),
+                        stop: 1 rgba({change_red}, {change_green}, {change_blue}, {self.opacity_animation * 5:.2f})
                     );
                     border-radius: 10px;
                 """)
@@ -375,7 +377,7 @@ class MyLoadingWindow(QWidget):
         """
 
         current_hour = datetime.now().hour
-        # current_hour = 23 # - отладка и тестирование
+        # current_hour = 12 # - отладка и тестирование
 
         debug_log(f"[I] текущее время {current_hour}")
 
